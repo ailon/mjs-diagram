428a1d08b2598c16fa8a1b509f4e9731fc1361b9
Alan Mendelevich <ailon@ailon.org>
2022-06-30 14:45:57 +0300

    add base DiagramEditor class

--- src/DiagramEditor.ts
+++ src/DiagramEditor.ts
@@ -1,3 +1,23 @@
 export class DiagramEditor extends HTMLElement {
+  private _container: HTMLDivElement;
 
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+
+    this.style.display = 'block';
+    this.style.width = '100%';
+    this.style.height = '100%';
+
+    this._container = document.createElement('div');
+    this._container.style.display = 'block';
+    this._container.style.width = '100%';
+    this._container.style.height = '100%';
+    this._container.style.backgroundColor = 'green';
+
+    this._container.setAttribute('part','container');
+
+    this.shadowRoot?.appendChild(this._container);  
+  }
 }
