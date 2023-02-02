b7d2577d8fbd2f4a91431adb7762b68263ebe223
Alan Mendelevich <ailon@ailon.org>
2023-02-02 15:37:56 +0200

    disable unneeded ports

--- src/stencilsets/mindmap/stencils/SubTopicStencil.ts
+++ src/stencilsets/mindmap/stencils/SubTopicStencil.ts
@@ -1,10 +1,22 @@
-import { TextStencil } from "../../../viewer_index";
+import { TextStencil } from '../../../viewer_index';
 
 export class SubTopicStencil extends TextStencil {
-    public static typeName = 'SubTopicStencil';
+  public static typeName = 'SubTopicStencil';
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public static getThumbnail(width: number, height: number): SVGSVGElement {
-        return super.getThumbnail(width, width / 4);
-    }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public static getThumbnail(width: number, height: number): SVGSVGElement {
+    return super.getThumbnail(width, width / 4);
+  }
+
+  constructor(iid: number, container: SVGGElement) {
+    super(iid, container);
+    this.disablePorts(
+      'topleft',
+      'topcenter',
+      'topright',
+      'bottomcenter',
+      'bottomleft',
+      'bottomright'
+    );
+  }
 }
