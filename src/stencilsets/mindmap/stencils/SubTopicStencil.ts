53f8f713c54b374d5392a4e6c388abe77423bc48
Alan Mendelevich <ailon@ailon.org>
2023-02-20 15:41:15 +0200

    change SubTopic to a ronded rectangle

--- src/stencilsets/mindmap/stencils/SubTopicStencil.ts
+++ src/stencilsets/mindmap/stencils/SubTopicStencil.ts
@@ -3,6 +3,23 @@ import { TextStencil } from "../../../core/TextStencil";
 export class SubTopicStencil extends TextStencil {
   public static typeName = 'SubTopicStencil';
 
+  protected static getPathD(width: number, height: number): string {
+    const r = 10;
+
+    const result = `M ${r} 0 
+      H ${width - r} 
+      A ${r} ${r} 0 0 1 ${width} ${r} 
+      V ${height - r} 
+      A ${r} ${r} 0 0 1 ${width - r} ${height} 
+      H ${r} 
+      A ${r} ${r} 0 0 1 0 ${height - r} 
+      V ${r} 
+      A ${r} ${r} 0 0 1 ${r} 0 
+      Z`;
+
+    return result;
+  }  
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public static getThumbnail(width: number, height: number): SVGSVGElement {
     return super.getThumbnail(width, width / 4);
