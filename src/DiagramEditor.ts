6c72d341c6ad6603992cd5b3d0e39749ccef2625
Alan Mendelevich <ailon@ailon.org>
2023-07-26 15:12:21 +0300

    change zoom in and out icons and swap button positions
    seems to be more common to have zoom-in on the right and out on the left

--- src/DiagramEditor.ts
+++ src/DiagramEditor.ts
@@ -814,14 +814,14 @@ export class DiagramEditor extends HTMLElement {
     });
     createBlock.appendButton(documentSetupButton);
 
-    const zoomInButton = new Button({
+    const zoomOutButton = new Button({
       icon: `<svg viewBox="0 0 24 24">
-    <path fill="currentColor" d="M15.5,14L20.5,19L19,20.5L14,15.5V14.71L13.73,14.43C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.43,13.73L14.71,14H15.5M9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14M12,10H10V12H9V10H7V9H9V7H10V9H12V10Z" />
+    <path fill="currentColor" d="M19,13H5V11H19V13Z" />
 </svg>`,
-      text: this.language.getString('toolbar-zoom-in') ?? 'zoom in',
-      command: 'zoomin',
+      text: this.language.getString('toolbar-zoomout') ?? 'zoom out',
+      command: 'zoomout',
     });
-    zoomBlock.appendButton(zoomInButton);
+    zoomBlock.appendButton(zoomOutButton);
 
     const zoomResetButton = new Button({
       icon: `<svg viewBox="0 0 24 24">
@@ -832,14 +832,14 @@ export class DiagramEditor extends HTMLElement {
     });
     zoomBlock.appendButton(zoomResetButton);
 
-    const zoomOutButton = new Button({
+    const zoomInButton = new Button({
       icon: `<svg viewBox="0 0 24 24">
-    <path fill="currentColor" d="M15.5,14L20.5,19L19,20.5L14,15.5V14.71L13.73,14.43C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.43,13.73L14.71,14H15.5M9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14M12,10H10V12H9V10H7V9H9V7H10V9H12V10Z" />
+    <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
 </svg>`,
-      text: this.language.getString('toolbar-zoomout') ?? 'zoom out',
-      command: 'zoomout',
+      text: this.language.getString('toolbar-zoom-in') ?? 'zoom in',
+      command: 'zoomin',
     });
-    zoomBlock.appendButton(zoomOutButton);
+    zoomBlock.appendButton(zoomInButton);
 
     const toolboxToggleButton = new Button({
       icon: `<svg viewBox="0 0 24 24">
