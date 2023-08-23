8fdde50d251910477f9b91f8e4e5d12865fa0384
Alan Mendelevich <ailon@ailon.org>
2023-08-23 10:50:51 +0300

    add custom image stencil thumbnail

--- src/core/CustomImageStencil.ts
+++ src/core/CustomImageStencil.ts
@@ -1,5 +1,6 @@
 import { DiagramSettings } from "./DiagramSettings";
 import { ImageStencil } from "./ImageStencil";
+import { SvgHelper } from "./SvgHelper";
 
 export class CustomImageStencil extends ImageStencil {
   public static typeName = 'CustomImageStencil';
@@ -7,6 +8,19 @@ export class CustomImageStencil extends ImageStencil {
   public static title = 'Custom image';
 
   protected static DEFAULT_TEXT = 'Image';
+
+  public static getThumbnail(width: number, height: number): SVGSVGElement {
+    const rectWidth = width * 0.9;
+    const rectHeight = Math.min(height * 0.9, rectWidth * 0.4);
+
+    const thumbnail = SvgHelper.createSvgFromString('<path d="M8.5,13.5L11,16.5L14.5,12L19,18H5M21,19V5C21,3.89 20.1,3 19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19Z" />');
+    SvgHelper.setAttributes(thumbnail, [
+      ['viewBox', '0 0 24 24'],
+      ['width', `${rectWidth}px`],
+      ['height', `${rectHeight}px`],
+    ]);
+    return thumbnail;
+  }
 
   /**
    * {@inheritDoc core!ConnectorBase.constructor}
