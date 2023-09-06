4c0ab36ed6f9cfe4480486d521ea0ff70491e96a
Alan Mendelevich <ailon@ailon.org>
2023-09-06 16:48:51 +0300

    update Lithuanian translation for core features

--- src/editor/lang/lt.ts
+++ src/editor/lang/lt.ts
@@ -1,8 +1,56 @@
 const strings: [string, string][] = [
   ['toolbar-select', 'pasirinkti'],
   ['toolbar-delete', 'ištrinti'],
-  
-  ['toolbox-shape-title', 'Objekto parametrai'],
+  ['toolbar-save', 'išsaugoti'],
+  ['toolbar-undo', 'atšaukti'],
+  ['toolbar-redo', 'pakartoti'],
+  ['toolbar-add', 'pridėti šabloną'],
+  ['toolbar-document-setup', 'dokumento savybės'],
+  ['toolbar-zoomin', 'priartinti'],
+  ['toolbar-zoomreset', 'atstatyti mastelį'],
+  ['toolbar-zoomout', 'nutolinti'],
+  ['toolbar-properties', 'perjungti įrankių juostą'],
+  ['toolbar-connect', 'sujungti'],
+
+  ['toolbox-shape-title', 'Figūros savybės'],
+  ['toolbox-linecolor-title', 'Linijos spalva'],
+  ['toolbox-fillcolor-title', 'Užpildymo spalva'],
+  ['toolbox-linewidth-title', 'Linijos plotis'],
+  ['toolbox-linestyle-title', 'Linijos stilius'],
+  ['toolbox-arrowtype-title', 'Rodyklės tipas'],
+
+  ['toolbox-text-title', 'Teksto savybės'],
+  ['toolbox-textcolor-title', 'Teksto spalva'],
+  ['toolbox-font-title', 'Šriftas'],
+  ['toolbox-increasefontsize-title', 'padidinti šrifto dydį'],
+  ['toolbox-decreasefontsize-title', 'sumažinti šrifto dydį'],
+
+  ['toolbox-image-title', 'Paveikslėlis'],
+  ['toolbox-label-location-title', 'Teksto vieta'],
+  ['toolbox-click-to-select-image', 'spustelėkite, kad pasirinktumėte paveikslėlį'],
+
+  ['toolbox-connectortype-title', 'Jungties tipas'],
+
+  ['toolbox-newstencil-title', 'Sukurti naują'],
+  ['toolbox-bgcolor-title', 'Fono spalva'],
+  ['toolbox-docsize-title', 'Dokumento dydis'],
+
+  ['toolbox-align-title', 'Lygiuoti'],
+  ['toolbox-align-left', 'Kairėje'],
+  ['toolbox-align-center', 'Centre'],
+  ['toolbox-align-right', 'Dešinėje'],
+  ['toolbox-align-top', 'Viršuje'],
+  ['toolbox-align-middle', 'Viduryje'],
+  ['toolbox-align-bottom', 'Apačioje'],
+
+  ['toolbox-arrange-title', 'Tvarkyti'],
+  ['toolbox-arrange-front', 'Perkelti į priekį'],
+  ['toolbox-arrange-forward', 'Perkelti į priekinį planą'],
+  ['toolbox-arrange-backward', 'Perkelti į galinį planą'],
+  ['toolbox-arrange-back', 'Perkelti į galą'],
+
+  ['toolbox-widthshort-label', 'P:'],
+  ['toolbox-heightshort-label', 'A:'],
 ];
 
 export default strings;
